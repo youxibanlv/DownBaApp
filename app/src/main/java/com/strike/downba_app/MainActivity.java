71b106101b139aa9840694398815de6a60682c24
xiaowuyue <653428992@qq.com>
2016-11-02 11:54:11 +0800

    添加gson  xutils 源码

--- app/src/main/java/com/strike/downba_app/MainActivity.java
+++ app/src/main/java/com/strike/downba_app/MainActivity.java
@@ -1,7 +1,9 @@
-package com.strike.downbaapp;
+package com.strike.downba_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.strike.downbaapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
